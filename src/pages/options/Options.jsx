141353a73cb7ffe94b11130e018bbff6a3570501
travis <trvsmartin@gmail.com>
2022-12-01 10:47:02 -0800

    display meals from backend data on options page
    <body>
    
    Issue #

--- src/pages/options/Options.jsx
+++ src/pages/options/Options.jsx
@@ -1,8 +1,27 @@
 import "./options.scss";
 
 import { Link } from "react-router-dom";
+import { useState } from "react";
+import axios from 'axios';
+import { useEffect } from "react";
+
+const BACK_END = process.env.REACT_APP_BACKEND_URL
 
 export default function Options() {
+const [recipes, setRecipes] = useState([]);
+// when a recipe is clicked on, set to active
+// const [activeRecipe, setActiveRecipe] = useState('')
+
+useEffect(()=>{
+  // on page load get recipes.  May end up moving this state up to app if search implemented; landing will set state and pass down
+  axios.get(`http://localhost:8080/recipes`)
+  .then((response)=>{
+    console.log(response.data)
+    setRecipes(response.data);
+    console.log(recipes)
+  })
+}, []);
+
   return (
     <div className="options__wrapper">
       <div className="options__left">
@@ -11,13 +30,13 @@ export default function Options() {
           <h3 className="options__meals-title">Meals to choose from</h3>
           {/* will map through options to generate this list */}
           <ul className="options__meals-list">
-            <li className="options__meals-item">meal1</li>
-            <li className="options__meals-item">meal2</li>
-            <li className="options__meals-item">meal3</li>
-            <li className="options__meals-item">meal4</li>
-            <li className="options__meals-item">meal5</li>
-            <li className="options__meals-item">meal6</li>
-          </ul>
+            {Object.keys(recipes).length > 0? 
+            recipes.map((recipe)=>(
+              <li key={recipe.idMeal} id={recipe.idMeal} className="options__meals-item">{recipe.strMeal}</li>
+            ))
+            :
+            <li>loading...</li>}
+            </ul>
         </div>
 
         {/* list of user selected meals */}
